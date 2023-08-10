0caa073f34b2156eef5620bcd81718b3baabe9d4
Willian <willian.alves.b15@gmail.com>
2023-08-09 21:39:24 -0300

    config: base do projeto configurada

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: { 
+      appDir: true,
+      optimizeCss: true, 
+    },
+    compiler: {
+      styledComponents: {
+        ssr: true
+      }
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/home',
+          permanent: true,
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
