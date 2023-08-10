42de4861eab768160814bcc24a78ab54116f39a1
Willian <willian.alves.b15@gmail.com>
2023-08-10 14:06:56 -0300

    feat: footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/Header";
 import Automobiles from "../../components/automobiles/Automobiles";
+import Footer from "../../components/footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Automobiles/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+"use client"
+import "./footer.css"
+
+
+export default function Footer() {
+  return(
+    <footer>
+      <p>Copyriht © 2023 Willian Alves Batista </p>
+    </footer>
+  );
+}
