348199c5339bdcd6f13e5fd4065c41df6aab2b9f
Willian <willian.alves.b15@gmail.com>
2023-08-10 10:18:16 -0300

    feat: mock de carros

---
+++ src/mockup/index.ts
@@ -0,0 +1,344 @@
+import cruze from "../assets/chevrolet_cruze.jpg";
+import onix from "../assets/chevrolet_onix.jpg";
+import prisma from "../assets/chevrolet_prisma.jpg";
+import argo from "../assets/fiat_argo.jpg";
+import mobi from "../assets/fiat_mobi.jpg";
+import pulse from "../assets/fiat_pulse.jpg";
+import siena from "../assets/fiat_siena.jpg";
+import strada from "../assets/fiat_strada.jpg";
+import uno from "../assets/fiat_uno.jpg";
+import hb20 from "../assets/hyundai_hb20.jpg";
+import hb20s from "../assets/hyundai_hb20s.jpg";
+import compass from "../assets/jeep_compass.jpg";
+import renegade from "../assets/jeep_renegade.jpg";
+import duster from "../assets/renault_duster.jpg";
+import logan from "../assets/renault_logan.jpg";
+import sandero from "../assets/renault_sandero.jpg";
+import corolla from "../assets/toyota_corolla.jpg";
+import hilux from "../assets/toyota_hilux.jpg";
+import gol from "../assets/volkswagen_gol.jpg";
+import voyage from "../assets/volkswagen_voyage.jpg";
+
+
+export const cars = [
+  {
+    img: cruze,
+    brand: "Chevrolet",
+    model: "Cruze",
+    description: "1.8 LT 16V FLEX 4P",
+    category: "Sedan",
+    year: 2016,
+    price: "65.990,00",
+    transmission: "Automático",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: onix,
+    brand: "Chevrolet",
+    model: "Onix",
+    description: "1.0 MPFI JOY 8V FLEX 4P",
+    category: "hatch",
+    year: 2019,
+    price: "54.740,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: prisma,
+    brand: "Chevrolet",
+    model: "Prisma",
+    description: "1.4 MPFI LT 8V FLEX 4P",
+    category: "Sedan",
+    year: 2019,
+    price: "69.790,00",
+    transmission: "Automática",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: argo,
+    brand: "Fiat",
+    model: "Argo",
+    description: "1.0 FIREFLY FLEX",
+    category: "Hatch",
+    year: 2019,
+    price: "52.000,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: mobi,
+    brand: "Fiat",
+    model: "Mobi",
+    description: "1.0 EVO FLEX LIKE",
+    category: "Hatch",
+    year: 2022,
+    price: "49.730,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: pulse,
+    brand: "Fiat",
+    model: "Pulse",
+    description: "1.3 FLEX DRIVE CVT",
+    category: "SUV",
+    year: 2022,
+    price: "105.900,00",
+    transmission: "Automático",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: siena,
+    brand: "Fiat",
+    model: "Siena",
+    description: "1.0 MPI EL 8V FLEX",
+    category: "Sedan",
+    year: 2015,
+    price: "35.900,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: strada,
+    brand: "Fiat",
+    model: "Strada",
+    description: "1.4 MPI WORKING CD 8V FLEX 3P",
+    category: "Picape",
+    year: 2015,
+    price: "64.990,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: uno,
+    brand: "Fiat",
+    model: "Uno",
+    description: "1.0 EVO WAY 8V FLEX 4P",
+    category: "Hatch",
+    year: 2014,
+    price: "34.990,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: hb20,
+    brand: "Hyundai",
+    model: "HB20",
+    description: "1.6 16V FLEX VISION",
+    category: "Hatch",
+    year: 2021,
+    price: "72.130,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: hb20s,
+    brand: "Hyundai",
+    model: "HB20S",
+    description: "1.6 COMFORT PLUS 16V FLEX 4P",
+    category: "Sedan",
+    year: 2015,
+    price: "51.900,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: compass,
+    brand: "Jeep",
+    model: "Compass",
+    description: "2.0 SPORT 4X2 16V GASOLINA 4P",
+    category: "SUV",
+    year: 2013,
+    price: "64.990,00",
+    transmission: "Automática",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: renegade,
+    brand: "Jeep",
+    model: "Renegade",
+    description: "1.8 16V FLEX LONGITUDE 4P",
+    category: "SUV",
+    year: 2018,
+    price: "81.992,00",
+    transmission: "Automática",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: duster,
+    brand: "Renault",
+    model: "Duster",
+    description: "1.6 EXPRESSION 4X2 16V FLEX 4P",
+    category: "SUV",
+    year: 2016,
+    price: "59.990,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: logan,
+    brand: "Renault",
+    model: "Logan",
+    description: "1.0 12V SCE FLEX LIFE",
+    category: "Sedan",
+    year: 2021,
+    price: "58.990,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: sandero,
+    brand: "Renault",
+    model: "Sandero",
+    description: "1.6 16V SCE FLEX ZEN",
+    category: "Hatch",
+    year: 2020,
+    price: "62.900,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: corolla,
+    brand: "Toyota",
+    model: "Corolla",
+    description: "1.8 GLI 16V FLEX 4P",
+    category: "Sedan",
+    year: 2019,
+    price: "92.950,00",
+    transmission: "Automático",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: hilux,
+    brand: "Toyota",
+    model: "Hilux",
+    description: "2.8 SRX 4X4 CD 16V DIESEL 4P",
+    category: "Picape",
+    year: 2018,
+    price: "184.990,00",
+    transmission: "Automática",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: gol,
+    brand: "Volkswagen",
+    model: "Gol",
+    description: "1.0 MI 8V FLEX 2P",
+    category: "Hatch",
+    year: 2014,
+    price: "31.899,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+  {
+    img: voyage,
+    brand: "Volkswagen",
+    model: "Voyage",
+    description: "1.6 MSI TOTALFLEX COMFORTLINE 4P",
+    category: "Sedan",
+    year: 2017,
+    price: "45.299,00",
+    transmission: "Manual",
+    characteristics: [
+      "Câmera de ré",
+      "Ar condicionado",
+      "Airbag",
+      "Central multimidia"
+    ],
+  },
+];
