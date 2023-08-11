9a28677022fc44299debbe5e4023513abdf99939
Willian <willian.alves.b15@gmail.com>
2023-08-10 21:12:46 -0300

    fix: mudando titulo do site

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ import head from 'next/head';
 export default function Head() {
     return (
       <head>
-        <title>Carros</title>
+        <title>Automóveis</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="Initial page" />
       </head>
