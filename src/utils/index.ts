36dec6b0b8a9b7b12d653246690f08b41984f683
Willian Batista <willian.alves.b15@gmail.com>
2024-04-16 21:57:11 -0300

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,9 +5,7 @@ export const handleCharactersFilter = (
   array: ICar[], filterString: string, filterYear: number, filterModel: string
   ) => {
 
-    if (filterString !== "") {
-      return array.filter((item) => item.brand === filterString);
-    }
+
   
     if (filterYear !== 0) {
       return array.filter((item) => item.year === filterYear);
