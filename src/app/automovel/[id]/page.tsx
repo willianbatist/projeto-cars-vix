9395c1f8822401d8a765e5c3609b413920baad9d
Willian Batista <willian.alves.b15@gmail.com>
2024-06-17 22:11:15 -0300

    Update page.tsx

--- src/app/automovel/[id]/page.tsx
+++ src/app/automovel/[id]/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "../../../components/footer/Footer";
 import Header from "../../../components/header/Header";
-import Automobile from "../../../components/automobile/Automobile";
-import Filter from "../../../components/filter/Filter";
+
 
 
 export default function DetailsCharacterId() {
